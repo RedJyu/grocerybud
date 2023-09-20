46d88e2a46b143ff6061c60ec08dbcc1b3ab69b0
RedJyu <piotr.stezycki@protonmai.com>
2023-09-20 15:17:19 +0200

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,37 @@ import Form from './Form';
 import { nanoid } from 'nanoid';
 import Items from './Items';
 
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
   const addItem = (itemName) => {
     const newItem = {
       name: itemName,
       completed: false,
       id: nanoid(),
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
   };
   return (
     <section className='section-center'>
